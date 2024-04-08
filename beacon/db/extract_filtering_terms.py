590d685373b0a8a8cd953e95a9f8bed416736c9b
Joakim Bygdell <joakim.bygdell@nbis.se>
2024-04-08 11:05:53 +0200

    Fix TLS momngoDB connection

--- beacon/db/extract_filtering_terms.py
+++ beacon/db/extract_filtering_terms.py
@@ -26,17 +26,21 @@ import conf
 
 ONTOLOGY_REGEX = re.compile(r"([_A-Za-z0-9]+):([_A-Za-z0-9^\-]+)")
 
-client = MongoClient(
-        #"mongodb://127.0.0.1:27017/"
-        "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
-            conf.database_user,
-            conf.database_password,
-            conf.database_host,
-            conf.database_port,
-            conf.database_name,
-            conf.database_auth_source,
-        )
-    )
+uri = "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
+    conf.database_user,
+    conf.database_password,
+    conf.database_host,
+    conf.database_port,
+    conf.database_name,
+    conf.database_auth_source
+)
+
+if conf.database_certificate:
+    uri += '&tls=true&tlsCertificateKeyFile={}'.format(conf.database_certificate)
+    if conf.database_cafile:
+        uri += '&tlsCAFile={}'.format(conf.database_cafile)
+
+client = MongoClient(uri)
 
 '''
 client = MongoClient(
